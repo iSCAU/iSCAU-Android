86cc12f7d5280fdbe90041e88fa8ee4fe29c45c9
Special Leung <specialcyci@gmail.com>
2014-05-07 21:11:13 +0800

    给查询已选选课增加缓存

--- app/src/main/java/cn/scau/scautreasure/ui/PickClassInfo.java
+++ app/src/main/java/cn/scau/scautreasure/ui/PickClassInfo.java
@@ -11,12 +11,16 @@ import org.androidannotations.annotations.UiThread;
 import org.androidannotations.annotations.rest.RestService;
 import org.springframework.web.client.HttpStatusCodeException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import cn.scau.scautreasure.AppContext;
 import cn.scau.scautreasure.R;
 import cn.scau.scautreasure.adapter.PickClassAdapter;
 import cn.scau.scautreasure.api.EdusysApi;
 import cn.scau.scautreasure.helper.UIHelper;
 import cn.scau.scautreasure.impl.ServerOnChangeListener;
+import cn.scau.scautreasure.util.CacheUtil;
 import cn.scau.scautreasure.widget.RefreshActionItem;
 
 import static cn.scau.scautreasure.helper.UIHelper.LISTVIEW_EFFECT_MODE.EXPANDABLE_ALPHA;
@@ -36,32 +40,27 @@ public class PickClassInfo extends CommonActivity implements ServerOnChangeListe
     EdusysApi api;
 
     private RefreshActionItem mRefreshActionItem;
+    private static final String cacheKey = "pickClassInfo";
 
     @AfterViews
     void init(){
         setTitle(R.string.title_pickclassinfo);
         setDataEmptyTips(R.string.tips_pickclassinfo_null);
-    }
-
-    @UiThread
-    void stopRefreing(){
-        mRefreshActionItem.stopProgress();
+        loadCacheData();
     }
 
     @Background( id = UIHelper.CANCEL_FLAG )
     void loadData(Object... params) {
         try{
             list = api.getPickClassInfo(AppContext.userName, app.getEncodeEduSysPassword(), AppContext.server).getPickclassinfos();
-            PickClassAdapter listadapter = new PickClassAdapter(getSherlockActivity(), R.layout.pickclassinfo_listitem, list);
-
-            adapter = UIHelper.buildEffectAdapter(listadapter, (AbsListView) listView,EXPANDABLE_ALPHA);
-            showSuccessResult();
+            writeCacheData((ArrayList) list);
+            buildAndShowAdapter(list);
         }catch (HttpStatusCodeException e){
             showErrorResult(getSherlockActivity(), e.getStatusCode().value(),this);
         }catch (Exception e){
             handleNoNetWorkError(getSherlockActivity());
         }
-        stopRefreing();
+        stopActionBarItemRefreing();
     }
 
     @Override
@@ -82,9 +81,55 @@ public class PickClassInfo extends CommonActivity implements ServerOnChangeListe
         return true;
     }
 
+
+    /**
+     * 停止 Action Bar Item 滚动。
+     */
+    @UiThread
+    void stopActionBarItemRefreing(){
+        mRefreshActionItem.stopProgress();
+    }
+
+    /**
+     * 点击 Action Bar 的刷新事件。
+     *
+     * @param refreshActionItem
+     */
     @Override
     public void onRefresh(RefreshActionItem refreshActionItem) {
         mRefreshActionItem.startProgress();
         loadData();
+    }
+
+
+    /**
+     * 构建 Adapter ， 并且刷新显示。
+     * @param l
+     */
+    private void buildAndShowAdapter(List l){
+        PickClassAdapter listadapter = new PickClassAdapter(getSherlockActivity(), R.layout.pickclassinfo_listitem, l);
+        adapter = UIHelper.buildEffectAdapter(listadapter, (AbsListView) listView,EXPANDABLE_ALPHA);
+        showSuccessResult();
+    }
+
+    /**
+     * 从硬盘加载缓存， 并且将数据显示出来。
+     */
+    void loadCacheData(){
+        CacheUtil cacheUtil = CacheUtil.get(getSherlockActivity());
+        ArrayList l = ( ArrayList ) cacheUtil.getAsObject(cacheKey);
+        if ( l != null)
+            buildAndShowAdapter(l);
+    }
+
+    /**
+     * 将数据写入到硬盘。
+     *
+     * @param list
+     */
+    @Background
+    void writeCacheData(ArrayList list){
+        CacheUtil cacheUtil = CacheUtil.get(getSherlockActivity());
+        cacheUtil.put(cacheKey, list);
     }
 }
