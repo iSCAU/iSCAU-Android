1ffa33fa6b63597b7bdbde7d20dcda8ccb969713
photoshit <qq2288446655@163.com>
2014-05-27 15:01:37 +0800

    Update NotificationReceiver.java

--- app/src/main/java/cn/scau/scautreasure/receiver/NotificationReceiver.java
+++ app/src/main/java/cn/scau/scautreasure/receiver/NotificationReceiver.java
@@ -7,32 +7,37 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.util.Log;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+
+import cn.scau.scautreasure.helper.UIHelper;
+import cn.scau.scautreasure.model.BookModel;
+import cn.scau.scautreasure.ui.CommonActivity;
+import cn.scau.scautreasure.util.CacheUtil;
 
 /**接收NotificationTiming广播后
  * 打开NotificationService
  * 避免直接操作Service会在应用管理程序中有显示
  * */
-
-public class NotificationReceiver extends BroadcastReceiver
-{
-//    String date1="2014-05-21";
-//    int year=2014;
-//    int mon=5;
-//    int day=23;
-    private String time[]=new String[10];
-    String now;
-
-    public void onReceive(Context context, Intent intent) {
-
-        Log.v("receiver","heheheaaa");
+public class NotificationReceiver extends BroadcastReceiver{
+     public void onReceive(Context context, Intent intent) {
         SharedPreferences share_date=context.getSharedPreferences("no_book",Activity.MODE_PRIVATE);
         int N=share_date.getInt("date",1);
 
+        String str=getNowDate(N);
+        boolean flag=matchRecode(str);
+
+        Log.v("test_dick",flag+"");
+        if(flag){
+          context.startService(new Intent(context, cn.scau.scautreasure.service.NotificationService.class));
+        }
+     }
+
+     public String getNowDate(int date){
         Calendar ca = Calendar.getInstance();
         int y = ca.get(Calendar.YEAR);//获取年份
         int m=ca.get(Calendar.MONTH)+1;//获取月份（android月份是从0开始的）
-        int dy=ca.get(Calendar.DATE)+N;//获取日
+        int dy=ca.get(Calendar.DATE)+date;//获取日
 
         String sm=m+"";
         if(m<10)
@@ -40,26 +45,34 @@ public class NotificationReceiver extends BroadcastReceiver
 
         String sdy=dy+"";
         if(dy<10)
-            sdy="0"+sdy;
+          sdy="0"+sdy;
 
-       now=y+"-"+sm+"-"+sdy;
-
-
+        String now=y+"-"+sm+"-"+sdy;
         Log.v("receiver",now);
 
-        SharedPreferences share=context.getSharedPreferences("no_book", Activity.MODE_PRIVATE);
-        int c=share.getInt("count",0);
-        for(int i=0;i<c&&i!=9;i++){
+        return  now;
+     }
 
-            time[i]=share.getString("time"+i,"0000-00-00");
-            time[i]=time[i].trim();
 
-            if(time[i].equals(now)) {
-                Log.v("nimabi","你麻痹为什么false");
-                context.startService(new Intent(context, cn.scau.scautreasure.service.NotificationService.class));
-                break;
-            }
+     public boolean matchRecode(String now){
+        boolean flag=false;
+        CommonActivity commonActivity=new CommonActivity();
+        commonActivity.setCacheKey("borrowedBook_" + UIHelper.TARGET_FOR_NOW_BORROW);
+        CacheUtil cacheUtil = CacheUtil.get(commonActivity.getSherlockActivity());
+        ArrayList list = (ArrayList) cacheUtil.getAsObject(commonActivity.getCacheKey());
+        if(list!=null) {
+          for (int i = list.size() - 1; i >= 0; i--) {
+               Log.v("list",  ((BookModel)list.get(i)).getShould_return_date());
+               String recode=((BookModel)list.get(i)).getShould_return_date().trim();
+               if(now.equals(recode)){
+                    flag=true;
+                    break;
+               }
+          }
+        }else{
+          flag=false;
         }
 
-    }
+        return flag;
+     }
 }
