76220527ea0032a17da4f137e9042bc80035fa5a
Macro2014 <macro621@vip.qq.com>
2014-09-05 13:41:05 +0800

    添加一个Download工具类,过渡版本校历更新

---
+++ app/src/main/java/cn/scau/scautreasure/api/CalendarApi.java
@@ -0,0 +1,16 @@
+package cn.scau.scautreasure.api;
+
+import android.content.Context;
+
+/**
+ * Created by apple on 14-9-5.
+ */
+//http://iscaucms-sto.stor.sinaapp.com/Calendar.json
+
+public class CalendarApi {
+    public static void setDir(){
+
+    }
+
+
+}
