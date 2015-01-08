fd252c89129343e5d5296e6d6f09a64d5ba5d696
attker <attker@126.com>
2015-01-08 21:56:43 +0800

    修改小细节

--- app/src/main/java/cn/scau/scautreasure/ui/Login.java
+++ app/src/main/java/cn/scau/scautreasure/ui/Login.java
@@ -106,7 +106,6 @@ public class Login extends Activity {
         app.config.libPassword().put(libPassword);
         app.config.cardPassword().put(cardPassword);
 
-        //LoginService_.intent(getApplicationContext()).start();
         UpLoadUsersService_.intent(getApplicationContext()).start();
 
         if (runMainActivity)
